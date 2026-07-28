109d85e1f3e4bf7d61be09c2f0cea4b30d65ada8
opgamer3455e <atharvninja123gamer@gmail.com>
2026-07-28 10:48:36 +0530

    Fix syntax nesting in AdminDashboard and finalize Orders CMS tab

--- src/pages/AdminDashboard.tsx
+++ src/pages/AdminDashboard.tsx
@@ -713,6 +713,7 @@ export default function AdminDashboard() {
                       </div>
                     ))}
                   </div>
+                </div>
               </div>
             </div>
           </div>
